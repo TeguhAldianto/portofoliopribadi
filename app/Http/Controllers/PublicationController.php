0ec61113f9b70c1d9bc9c47a380fba50f5b2fc7d
Teguh Aldianto <aldinamanya08@gmail.com>
2026-03-04 16:40:16 +0700

    publicationcontroller di update

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -9,11 +9,11 @@ class PublicationController extends Controller
     public function index()
     {
         // Ambil artikel khusus yang di-"feature"
-        $featuredPublication = \App\Models\Publication::where('is_featured', true)->first();
+        $featuredPublication = Publication::where('is_featured', true)->first();
 
         // Ambil semua artikel lain yang BUKAN featured
-        $publications = \App\Models\Publication::where('is_featured', false)
-            ->orderBy('created_at', 'desc') // atau orderBy date
+        $publications = Publication::where('is_featured', false)
+            ->orderBy('created_at', 'desc')
             ->get();
 
         return view('publications', compact('featuredPublication', 'publications'));
